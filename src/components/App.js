0f2935ad52c1057eb3537e05d69be802326f3c78
David Zukowski <david@zuko.me>
2018-04-15 22:42:20 -0500

    Feat: setup preact and parcel

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import { h, Component } from 'preact'
+
+class App extends Component {
+  render() {
+    return <h1>Hello Jackie</h1>
+  }
+}
+
+export default App
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('hello netlify')
+import { h, render } from 'preact'
+import App from './components/App'
+import './styles/main.css'
+
+const mountApp = () => {
+  render('', document.body, window.__root__)
+  window.__root__ = render(<App />, document.body)
+}
+
+mountApp()
