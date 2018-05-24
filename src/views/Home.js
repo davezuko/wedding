67a3ed125d8df99b9fb40b2127d1aa0ae9ac4775
David Zukowski <6439050+davezuko@users.noreply.github.com>
2018-05-24 10:04:56 -0500

    Update Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -9,16 +9,17 @@ class Home extends React.Component {
         <p>
           Fate brought Jackie and David together in the fall of 2014 in true
           modern day fashion, on the trusty dating app, OkCupid. David had just
-          finished school at Western Michigan University and moved into his
+          finished school at Western Michigan University and had moved into his
           first apartment in Birmingham, Michigan. Jackie was a social work
-          student at Oakland University and worked as a soccer coach at the
+          student at Oakland University at the time and worked as a soccer coach at the
           time.
         </p>
         <p>
           Not long after they started chatting, David picked Jackie up for their
-          first date they went to a local park to eat a Little Ceasers pizza and
-          get to know each other. Jackie was impressed with David’s intelligence
-          and sense of adventure. They quickly realised that they had grown up
+          first date. They went to a local park to eat a Little Ceasers pizza and
+          get to know each other. After a few hours of riveting conversation, they 
+          headed to downtown Rochester to walk along the river. They quickly realized
+          that they had grown up
           less than a quarter mile from each other.
         </p>
       </div>
